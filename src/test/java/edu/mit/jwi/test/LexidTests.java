774c5c06332b37c16ad9d470a6bc73378f899274
1313ou <1313ou@gmail.com>
2023-07-14 09:40:16 +0200

    fix output

--- src/test/java/edu/mit/jwi/test/LexidTests.java
+++ src/test/java/edu/mit/jwi/test/LexidTests.java
@@ -21,13 +21,13 @@ public class LexidTests
 	public void parseCompatLexid()
 	{
 		String line = "02504828 00 s 01 hot 03 001 & 02504619 a 0000 | (color) bold and intense; \"hot pink\"";
-		TestLib.parseDataLineIntoMembers(line).forEach(System.out::println);
+		TestLib.parseDataLineIntoMembers(line).forEach(PS::println);
 	}
 
 	@Test
 	public void parseNonCompatLexid()
 	{
 		String line = "02504828 00 s 01 hot 13 001 & 02504619 a 0000 | (color) bold and intense; \"hot pink\"";
-		TestLib.parseDataLineIntoMembers(line).forEach(System.out::println);
+		TestLib.parseDataLineIntoMembers(line).forEach(PS::println);
 	}
 }
