4440ce84cee17bad112ad8054ecad2d396479079
1313ou <1313ou@gmail.com>
2023-07-14 17:42:36 +0200

    lint

--- src/main/java/edu/mit/jwi/item/WordID.java
+++ src/main/java/edu/mit/jwi/item/WordID.java
@@ -216,14 +216,7 @@ public class WordID implements IWordID
     {
         assert id != null;
         return 31 * id.hashCode();
-        // final int PRIME = 31;
-        // int result = 1;
-        // result = PRIME * result + ((id == null) ? 0 : id.hashCode());
-        // result = PRIME * result + num;
-        // if (lemma != null)
-        // 	result = PRIME * result + lemma.hashCode();
-        // return result;
-    }
+     }
 
     /*
      * (non-Javadoc)
